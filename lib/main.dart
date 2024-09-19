83ff41ba4d0fd0dc1d8b2da4987a5f49e9fec92a
Bircan E. <bircan.emtekin@hotmail.com>
2024-09-19 22:59:19 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
     const MaterialApp(
       home: Scaffold(
         body: GradiantContainer(
-          Color.fromARGB(255, 48, 17, 102),
+          Color.fromARGB(255, 51, 16, 110),
           Color.fromARGB(255, 99, 73, 145),
         ),
       ),
